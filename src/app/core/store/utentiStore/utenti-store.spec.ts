53920fbff96ec374321c1000307bd7b554fa70db
Christian Giaccardi <christianerita@Mac.lan>
2026-05-23 08:26:47 +0200

    Elaborazione Testing UtentiStore

--- src/app/core/store/utentiStore/utenti-store.spec.ts
+++ src/app/core/store/utentiStore/utenti-store.spec.ts
@@ -1,6 +1,10 @@
 // TestBed serve a creare l'ambiente di test
 import { TestBed } from "@angular/core/testing";
 import { UtentiStore } from "./utenti-store";
+import { Utente } from "../../models/utente";
+import { provideHttpClient } from "@angular/common/http";
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing'
+import { firstValueFrom } from "rxjs";
 
 // describe raggruppa i test che appartengono allo stesso argomento
 // Test che riguardano utentiStore
@@ -11,17 +15,28 @@ describe('UtentiStore', () => {
     // Typeof la tipologia
     // InstanceType tipo di oggetto che ricevo quando angular lo inietta
     let store: InstanceType<typeof UtentiStore>;
+    let httpTesting: HttpTestingController
 
     // Blocco eseguito prima di ogni Test it, non lo ripetiamo sempre,
     // ed a ogni test abbiamo l'ambiente pulito
     beforeEach(() => {
         // Si configura l'ambiente con il provider così angular sa dove recuperarlo
         TestBed.configureTestingModule({
-            providers: [UtentiStore]
+            providers: [
+                UtentiStore,
+                provideHttpClient(),
+                provideHttpClientTesting()
+            ]
         });
         // Recuperiamo UtentiStore e lo assegnamo a store
         store = TestBed.inject(UtentiStore);
+        httpTesting = TestBed.inject(HttpTestingController)
     });
+
+    // Controlliamo che alla fine non rimangono richieste gttp pendenti
+    afterEach(() => {
+        httpTesting.verify();
+    })
 
     // Test: partiamo dai più semplici ad arrivare a quelli più complicati
     it('Dovrebbe essere Creato', () => {
@@ -44,12 +59,43 @@ describe('UtentiStore', () => {
     it('setIdUtente imposta selezioneIdUtente', () => {
         store.setIdUtente('842545')
         expect(store.selezioneIdUtente()).toBe('842545')
-    })
+    });
 
     it('paginaPrecedente / paginaSuccessiva aggiornano paginaCorrente', () => {
         store.andareAPagina(3)
         expect(store.paginaPrecedente()).toBe(2)
         expect(store.paginaSuccessiva()).toBe(4)
+    });
+
+    it('controlloAdmin se esiste return, se non esiste crea',
+        async () => {
+        // Creiamo un adminFinto da poter utilizzare
+        const adminFinto: Utente = {
+            id: '1',
+            name: 'Admin',
+            email: 'admincitysharehub@admin.it',
+            gender: 'male',
+            status: 'active',
+        }
+         
+        // Istanziamo il primo risultato che arriva dal metodo
+        const risultatoPromise = firstValueFrom(store.controlloAdmin());
+
+        // Intercettiamo la richiesta e mi aspetto che sia fatta con il metodo GET
+        const richiesta = httpTesting.expectOne((richiesta) => richiesta.method === 'GET')
+      
+        // Intercettata la richiesta mettiamo come risultato con .flush un array con admin finto dentro.
+        // In questo modo cerca admin restituisce questo visto che deve restituire un Observable <Utente[]>
+        richiesta.flush([adminFinto])
+            
+        // A sto punto il risultato è l'await di risultatoPromise
+        const risultatoFinale = await risultatoPromise
+        
+        // In conclusione ci aspettiamo che il risultato finale sia uguale ad adminFinto
+        expect(risultatoFinale).toEqual(adminFinto)
+            
+
+
     })
 
     
