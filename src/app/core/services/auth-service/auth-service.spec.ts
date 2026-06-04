7cbd624a103389a75beb5b9f55c7eda2c3ac81ab
Christian Giaccardi <christianerita@Mac.lan>
2026-06-04 22:35:24 +0200

    Elaborazione Post Card Spec

--- src/app/core/services/auth-service/auth-service.spec.ts
+++ src/app/core/services/auth-service/auth-service.spec.ts
@@ -2,11 +2,12 @@ import { TestBed } from '@angular/core/testing';
 
 import { AuthService } from './auth-service';
 import { provideRouter, Router } from '@angular/router';
-import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 
 describe('AuthService', () => {
   let service: AuthService;
   let router: Router
+  let httpTesting: HttpTestingController
 
   // Creiamo describe separati visto il test del costruttore
   describe('Constructor - Token presente', () => {
@@ -39,6 +40,29 @@ describe('AuthService', () => {
     })
   })
 
+  describe('Constructor - Token assente', () => {
+    let serviceNoToken: AuthService
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        providers: [
+          AuthService,
+          provideHttpClientTesting(),
+          provideRouter([])
+        ]
+      });
+      localStorage.removeItem('tokenDiAccesso')
+      serviceNoToken = TestBed.inject(AuthService)
+    })
+    afterEach(() => {
+      localStorage.clear()
+    })
+
+    it('Dovrebbe lasciare token null e utenteLoggato false', () => {
+      expect(serviceNoToken.token()).toBe(null)
+      expect(serviceNoToken.utenteLoggato()).toBe(false)
+    })
+  })
+
   describe('Altri Test AuthService', () => {
 
     beforeEach(() => {
@@ -50,7 +74,8 @@ describe('AuthService', () => {
         ]
       });
       service = TestBed.inject(AuthService);
-      router = TestBed.inject(Router)
+      router = TestBed.inject(Router);
+      httpTesting = TestBed.inject(HttpTestingController);
     });
 
     it('should be created', () => {
@@ -75,6 +100,30 @@ describe('AuthService', () => {
       expect(service.codiceErrore()).toBe(null)
       expect(localStorage.getItem('tokenDiAccesso')).toBe(null)
       expect(navigateSpy).toHaveBeenCalledWith(['/login'])
+    })
+
+    it('verificaToken con 200 dovrebbe salvare il token e navigare verso dashboard', () => {
+      // definisco i valori prima del metodo
+      const tokenLS = '123abc'
+      service.codiceErrore.set(401)
+      // Spiamo la navigazione
+      const navigateSpy = vi.spyOn(router, 'navigate').mockResolvedValue(true)
+      // Facciamo partire il metodo
+      service.verificaBearerToken(tokenLS)
+      // Intercettiamo la richiesta che ha come metodo GET e url.utenti
+      const richiestaGET = httpTesting.expectOne({ method: 'GET', url: service.urlUtenti })
+      // Diamo come risposta della richiesta stato 200
+      richiestaGET.flush('Risposta positiva', {
+        status: 200,
+        statusText: 'Token Valido'
+      })
+      // Controlliamo che i valori finali siano giusti
+      expect(service.token()).toBe(tokenLS)
+      expect(service.utenteLoggato()).toBe(true)
+      expect(localStorage.getItem('tokenDiAccesso')).toBe(tokenLS)
+      expect(navigateSpy).toHaveBeenCalledWith(['/dashboard'])
+      expect(service.codiceErrore()).toBe(null)
+      expect(richiestaGET.request.method).toBe('GET')
     })
 
   })
--- src/app/features/dashboard/pages/lista-post/components/card-post/card-post.spec.ts
+++ src/app/features/dashboard/pages/lista-post/components/card-post/card-post.spec.ts
@@ -4,18 +4,28 @@ import { CardPost } from './card-post';
 import { provideRouter } from '@angular/router';
 import { signal } from '@angular/core';
 import { PostsStore } from '../../../../../../core/store/postsStore/posts-store';
+import { By } from '@angular/platform-browser';
 
 describe('CardPost', () => {
   let component: CardPost;
   let fixture: ComponentFixture<CardPost>;
 
-  // Mockiamo il postStore per non fare la richiesta HTTP
+  // Mockiamo gli store 
   const postsStoreMock = {
     getNomeUtente: vi.fn().mockReturnValue('Mario Rossi'),
     caricamento: signal(false),
     errore: signal(undefined),
     posts: signal([]),
   };
+  const commentiStoreMock = {
+    postIdSelezionato: signal(null),
+    numeroCommenti: signal(undefined),
+    caricamentoCommenti: signal(false),
+    erroreCommenti: signal(undefined),
+    commentiPost: signal([]),
+    setIdPost: vi.fn()
+  };
+
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -26,6 +36,7 @@ describe('CardPost', () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(CardPost);
+    // Impostiamo in input un mockPost per non avere errori nel test
     component = fixture.componentInstance;
     fixture.componentRef.setInput('post', {
       id: '2',
@@ -39,4 +50,15 @@ describe('CardPost', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Dovrebbero visualizzarsi titolo e body', () => {
+    // Applichiamo i cambiamenti
+    fixture.detectChanges()
+    // Selezioniamo h2 per il titolo e p per il body
+    const titolo = fixture.debugElement.query(By.css('h2'))
+    const body = fixture.debugElement.query(By.css('p'))
+    
+    expect(titolo.nativeElement.textContent).toContain('Test Post')
+    expect(body.nativeElement.textContent).toContain('Body Post')
+  })
 });
