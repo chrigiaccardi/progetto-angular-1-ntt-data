1186c632adc3971de8ba85d88737095fafddd436
Christian Giaccardi <christianerita@Mac.lan>
2026-06-04 07:50:58 +0200

    Test Coverage e sistemazione altri test

--- src/app/core/services/auth-service/auth-service.spec.ts
+++ src/app/core/services/auth-service/auth-service.spec.ts
@@ -13,4 +13,15 @@ describe('AuthService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('Logout dovrebbe cancellare utente loggato', () => {
+    // Definisco il valore di tokenS
+    const tokenS = '123abc'
+    // Creo un mock come se l'utente fosse loggato
+    service.token.set(tokenS)
+    service.utenteLoggato.set(true)
+    service.codiceErrore.set(401)
+    localStorage.setItem('tokenDiAccesso', tokenS)
+
+  })
 });
